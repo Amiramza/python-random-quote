a4805735d2bafc31535655b370d50b701fd12405
Amir Hamza <81413226+Amiramza@users.noreply.github.com>
2022-03-11 23:06:29 +0500

    Basic matplotlib plot

---
+++ plt.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+plt.scatter([i for i in range(10)],[i for i in range(10)],alpha=1)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
